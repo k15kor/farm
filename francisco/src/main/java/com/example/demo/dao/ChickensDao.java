f0c8f5afacf1ef18dc219c5d3ec159d86580abfb
k15kor <k15kor@hotmail.com>
2017-09-14 15:30:17 +0200

    estructura del DAO sin relacionar
    Clases Users, Chickens y Production con los métodos geter y seter

---
+++ francisco/src/main/java/com/example/demo/dao/ChickensDao.java
@@ -0,0 +1,27 @@
+package com.example.demo.dao;
+
+public class ChickensDao {
+
+	private String id;
+	private int id_user;
+	private String frecuency;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getId_user() {
+		return id_user;
+	}
+	public void setId_user(int id_user) {
+		this.id_user = id_user;
+	}
+	public String getFrecuency() {
+		return frecuency;
+	}
+	public void setFrecuency(String frecuency) {
+		this.frecuency = frecuency;
+	}
+			
+}
