a39292276818deb2d949efad33a41600dc320130
k15kor <k15kor@hotmail.com>
2017-09-16 12:20:37 +0200

    comento log...
    comento 		log.info("Intentando crear un usuario");
     aun teniendo el lombok da fallo

--- francisco/src/main/java/com/example/demo/controller/UserController.java
+++ francisco/src/main/java/com/example/demo/controller/UserController.java
@@ -25,35 +25,35 @@ public class UserController implements CRUD<User, Integer> {
 	@Override
 	@RequestMapping(method = RequestMethod.POST)
 	public User create(@RequestBody User t) {
-		log.info("Intentando crear un usuario");
+		//log.info("Intentando crear un usuario");
 		return userService.create(t);
 	}
 
 	@Override
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public void delete(@RequestBody User t) {
-		log.info("Vamos a borrar");
+		//log.info("Vamos a borrar");
 		userService.delete(t);
 	}
 
 	@Override
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public void update(@RequestBody User t) {
-		log.info("Vamos a actualizar");
+		//log.info("Vamos a actualizar");
 		userService.update(t);
 	}
 
 	@Override
 	@RequestMapping(method = RequestMethod.GET)
 	public List<User> getAll() {
-		log.info("Vamos a recuperar todos");
+		//log.info("Vamos a recuperar todos");
 		return userService.getAll();
 	}
 
 	@Override
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public User findById(@PathVariable("id") Integer id) {
-		log.info("Vamos a recuperar un usuario con id " + id);
+		//log.info("Vamos a recuperar un usuario con id " + id);
 		return userService.findById(id);
 	}
 
